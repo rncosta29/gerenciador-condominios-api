d735bb52fcd5a1781aa6cd5e6ca543bd91297e47
Rafael Costa <raf.costa29@gmail.com>
2021-05-14 16:30:22 -0300

    Projeto Iniciado

---
+++ src/main/java/br/com/devcosta/gerenciador/GerenciadorCondominiosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.devcosta.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCondominiosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorCondominiosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devcosta/gerenciador/GerenciadorCondominiosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devcosta.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorCondominiosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
